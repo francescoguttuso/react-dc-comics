4ae9b1c98b756801de7d1ca46fc160f8a574c933
francesco-guttuso <francescoguttuso@gmail.com>
2026-07-01 23:18:30 +0200

    feat: enhance Header component with Logo and Navbar; update App component structure; add CSS for Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,12 @@ import "./App.css";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
-const App = () => {
+const App = () => (
   <>
     <Header />
     <Main />
     <Footer />
-  </>;
-  return (
-    <>
-      <h1>Progetto DC Comics Inizializzato</h1>
-    </>
-  );
-};
+  </>
+);
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,27 @@
-export const Header = () => <> </>;
+import "./Header.css";
+
+const Logo = () => <img src="img/dc-logo.png" alt="Logo" />;
+const Navbar = () => (
+  <ul>
+    <li>
+      <a href="#">CHARACTERS</a>
+      <a href="#" className="active">
+        COMICS
+      </a>
+      <a href="#">MOVIES</a>
+      <a href="#">TV</a>
+      <a href="#">GAMES</a>
+      <a href="#">COLLECTIBLES</a>
+      <a href="#">VIDEOS</a>
+      <a href="#">FANS</a>
+      <a href="#">NEWS</a>
+      <a href="#">SHOP</a>
+    </li>
+  </ul>
+);
+export const Header = () => (
+  <header>
+    <Logo />
+    <Navbar />
+  </header>
+);
