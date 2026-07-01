21957285524ed05187a2611e61d7feb820a2ca2f
francesco-guttuso <francescoguttuso@gmail.com>
2026-07-01 22:25:10 +0200

    fix: update App component to use named imports for Header, Main, and Footer; add empty CSS files for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-
-function App() {
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+const App = () => {
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>;
   return (
     <>
       <h1>Progetto DC Comics Inizializzato</h1>
     </>
   );
-}
+};
 
 export default App;
