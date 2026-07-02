f447e659f5bd15ff3b0e7de35b0d5681b27c3731
francescoguttuso <francescoguttuso@gmail.com>
2026-07-02 12:32:59 +0200

    feat: implement BluBar component with layout and styling

--- src/components/BluBar.jsx
+++ src/components/BluBar.jsx
@@ -1,3 +1,37 @@
 import "./BluBar.css";
 
-export const BluBar = () => <></>
+export const BluBar = () => {
+    return (
+        <div className="blu-bar">
+            <div className="container">
+                <ul>
+                    <li>
+                        <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" />
+                        <span>DIGITAL COMICS</span>
+                    </li>
+
+                    <li>
+                        <img src="/img/buy-comics-merchandise.png" alt="Merchandise" />
+                        <span>DC MERCHANDISE</span>
+                    </li>
+
+                    <li>
+                        <img src="/img/buy-comics-subscriptions.png" alt="Subscription" />
+                        <span>SUBSCRIPTION</span>
+                    </li>
+
+                    <li>
+                        <img src="/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" />
+                        <span>COMIC SHOP LOCATOR</span>
+                    </li>
+
+                    <li>
+                        <img src="/img/buy-dc-power-visa.svg" alt="DC Power Visa" />
+                        <span>DC POWER VISA</span>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    );
+};
