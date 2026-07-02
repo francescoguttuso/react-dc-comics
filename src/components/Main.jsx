774220255ed1720f14036a249770c4aee3506240
francesco-guttuso <francescoguttuso@gmail.com>
2026-07-02 22:38:21 +0200

    feat: add comics data and import in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,12 @@
 import "./Main.css";
+import "./data/comics.js";
 
 export const Main = () => {
-    return (
-        <main>
-            <div className="content-placeholder">
-                <h2>--&gt; Content goes here &lt;--</h2>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div className="content-placeholder">
+        <h2>--&gt; Content goes here &lt;--</h2>
+      </div>
+    </main>
+  );
 };
