31ed3f3051e963c8a987283656cfb15eb5b76616
francescoguttuso <francescoguttuso@gmail.com>
2026-07-02 12:20:36 +0200

    feat: add BluBar component and integrate it into the App

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
+import { BluBar } from "./components/BluBar";
 
 const App = () => (
   <>
     <Header />
     <Main />
+    <BluBar />
     <Footer />
   </>
 );
---
+++ src/components/BluBar.jsx
@@ -0,0 +1,3 @@
+import "./BluBar.css";
+
+export const BluBar = () => <></>
