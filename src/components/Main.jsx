04f477132ab45901fa9aaa818e5ced5f3b8bacb3
francescoguttuso <francescoguttuso@gmail.com>
2026-07-02 08:12:27 +0200

    fix: update Main component to render content instead of returning empty fragment

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,1 +1,10 @@
-export const Main = () => <></>;
+
+export const Main = () => {
+    return (
+        <main>
+            <div className="content-placeholder">
+                <h2>--&gt; Content goes here &lt;--</h2>
+            </div>
+        </main>
+    );
+};
