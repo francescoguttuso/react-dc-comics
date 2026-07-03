5d0e7ee86e0a827a6023b2d14468d221f15fb599
francesco-guttuso <francescoguttuso@gmail.com>
2026-07-03 23:25:58 +0200

    style: update layout and styles for BluBar, Footer, and Main components for improved consistency and responsiveness

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import "./Main.css";
 import comics from "../data/comics.js";
 
+const Jumbotron = () => <div className="jumbotron" />;
+
 export const Main = () => {
   return (
     <main>
       <div className="container">
-        <img src="img/jumbotron.jpg" alt="jumbotron" />
-        <p>current series</p>
+        <Jumbotron />
+        <h1>current series</h1>
       </div>
     </main>
   );
